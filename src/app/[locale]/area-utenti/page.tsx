385833a85e8e29c2e170c4d14ed6b20944e602a1
grafica <paolo@monema.it>
2024-10-06 18:37:43 +0200

    test pagina utenti

---
+++ src/app/[locale]/area-utenti/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        Benvenuto nell&apos;area utenti. Inserisci l&apos;indirizzo sulla barra
+        di navigazione del tuo browser
+      </h1>
+    </div>
+  );
+}
+
+export default Page;
