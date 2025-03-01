8c75c4dccf113e652a7732aa9c7e9cde95b25d86
grafica <paolo@monema.it>
2025-03-01 12:02:47 +0100

    cookie consent aggiornato

---
+++ src/components/cookieConsent/googleAnalytics.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Script from "next/script";
+import cookie from "js-cookie";
+export default function GoogleAnalytics({
+  GA_MEASUREMENT_ID,
+}: {
+  GA_MEASUREMENT_ID: string;
+}) {
+  const cookieConsent = cookie.get("cookieConsent");
+  if (!cookieConsent || cookieConsent === "rejected") {
+    return null;
+  } else {
+    return (
+      <>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('consent', 'default', {
+                    'analytics_storage': 'granted'
+                });
+                
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                });
+                `,
+          }}
+        />
+      </>
+    );
+  }
+}
